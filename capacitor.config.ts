1bee5265809bb40193f7687f870d3c71a568f8a9
[dyad] <git@dyad.sh>
2025-09-25 18:29:53 -0300

    Reverted all changes back to version e98f189d4288bf4823a1b26c0ba3098851d923ce

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.dreduardoquiztesteswiftsalamanderplay',
-  appName: 'Dr Eduardo QUIZ TESTE swift-salamander-play',
-  webDir: 'dist'
-};
-
-export default config;
